059aa545308b018071d6edf04fc87f506fb7d25f
Pieter-Jan Verhulst <pieterjanverhulst@hotmail.com>
2016-12-23 15:43:02 +0000

    Fixed bug for Heroku deployment

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ActionMailer::Base
-    default to 'pieterjanverhulst@hotmail.com'
+    default to: 'pieterjanverhulst@hotmail.com'
     
     def contact_email(name, email, body)
         @name = name
